18e2416a72f5dc7471282eb3aeebd37cf4c770ac
Slava Tiutiunnyk <gloryon87@gmail.com>
2023-02-02 14:18:43 +0200

    js1 homework

---
+++ script/script.js
@@ -0,0 +1,8 @@
+let admin;
+const userName="Слава";
+admin=userName;
+console.log(userName);
+document.write(`Привіт, ${userName}!`);
+const days=2;
+console.log(`${days*24*60*60} seconds`);
+console.log(prompt(`${userName}, скільки Вам років?`));
