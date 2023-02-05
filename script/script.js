9629e707b2e6419d301a1e536ddbad7938a0f251
Slava Tiutiunnyk <gloryon87@gmail.com>
2023-02-05 08:55:14 +0200

    js homework2

--- script/script.js
+++ script/script.js
@@ -1,9 +1,25 @@
-"use strict";
-let admin;
-const userName="Слава";
-admin=userName;
-console.log(userName);
-document.write(`Привіт, ${userName}!`);
-const days=2;
-console.log(`${days*24*60*60} seconds`);
-console.log(prompt(`${userName}, скільки Вам років?`));
+'use strict'
+let userAge = prompt('Your age?')
+while (userAge !== null && (isNaN(userAge) || +userAge === 0)) {
+  userAge = prompt('Your age?', `${userAge}`)
+}
+if (+userAge >= 22) {
+} else if (+userAge >= 18) {
+  if (confirm('Are you sure you want to continue?')) {
+  } else {
+    alert('You are not allowed to visit this website.')
+    exit
+  }
+} else {
+  alert('You are not allowed to visit this website.')
+  exit
+}
+let userName = prompt('Your name')
+while (userName !== null && userName === "") {
+  userName = prompt('Your name', `${userName}`)
+}
+if (userName === null) {
+  alert('You are not allowed to visit this website.')
+  exit
+}
+alert(`Welcome, ${userName}`)
