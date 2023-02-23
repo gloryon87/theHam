a5d0ce95edb58a7ec305548763bdd968110aeac3
Slava Tiutiunnyk <gloryon87@gmail.com>
2023-02-23 22:04:18 +0200

    js homework 6

--- script/script.js
+++ script/script.js
@@ -1,18 +1,66 @@
-"use strict"
+'use strict'
 
-let userNumber;
+/**
+ * Creates new user
+ * @param {string} firstName
+ * @param {string} lastName
+ * @returns {object}
+ **/
+const createNewUser = (firstName, lastName) => {
+  let date
+  do {
+    date = prompt('Your birthday, dd.mm.yyyy')
+  } while (isNaN(parseInt(date)) && date !== null)
 
-do {userNumber = prompt("Enter Number", 0)}
-while (userNumber !== null && isNaN(parseInt(userNumber)));
-
-const calculateFactorial = userNumber => {
-    if (userNumber < 0)
-    return -1;
-    else if (userNumber == 0)
-    return 1;
-    else {
-        return (userNumber * calculateFactorial(userNumber - 1));
+  const newUser = {
+    _firstName: firstName,
+    _lastName: lastName,
+    birthday: new Date(
+      date.split('.')[1] + '.' + date.split('.')[0] + '.' + date.split('.')[2]
+    ),
+    getLogin () {
+      return (
+        this._firstName.charAt(0).toLowerCase() + this._lastName.toLowerCase()
+      )
+    },
+    setFirstName (firstName) {
+      Object.defineProperty(newUser, '_firstName', {
+        writable: true
+      })
+      this._firstName = firstName
+      Object.defineProperty(newUser, '_firstName', {
+        writable: false
+      })
+    },
+    setLastName (lastName) {
+      Object.defineProperty(newUser, '_lastName', {
+        writable: true
+      })
+      this._lastName = lastName
+      Object.defineProperty(newUser, '_lastName', {
+        writable: false
+      })
+    },
+    getAge () {
+      return parseInt((new Date() - this.birthday) / 31536000000)
+    },
+    getPassword () {
+      return (
+        this._firstName[0].toUpperCase() +
+        this._lastName.toLowerCase() +
+        this.birthday.getFullYear()
+      )
     }
+  }
+
+  Object.defineProperty(newUser, '_firstName', {
+    writable: false
+  })
+
+  Object.defineProperty(newUser, '_lastName', {
+    writable: false
+  })
+  console.log(newUser, newUser.getAge(), newUser.getPassword())
 }
 
-alert(calculateFactorial(userNumber));
+createNewUser('Vasia', 'Pupkin')
