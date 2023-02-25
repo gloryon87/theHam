984e1f53f589b58ba504ea648b3cfe0391557254
Slava Tiutiunnyk <gloryon87@gmail.com>
2023-02-25 11:14:43 +0200

    js homework 7

--- script/script.js
+++ script/script.js
@@ -1,66 +1,39 @@
 'use strict'
 
-/**
- * Creates new user
- * @param {string} firstName
- * @param {string} lastName
- * @returns {object}
- **/
-const createNewUser = (firstName, lastName) => {
-  let date
-  do {
-    date = prompt('Your birthday, dd.mm.yyyy')
-  } while (isNaN(parseInt(date)) && date !== null)
-
-  const newUser = {
-    _firstName: firstName,
-    _lastName: lastName,
-    birthday: new Date(
-      date.split('.')[1] + '.' + date.split('.')[0] + '.' + date.split('.')[2]
-    ),
-    getLogin () {
-      return (
-        this._firstName.charAt(0).toLowerCase() + this._lastName.toLowerCase()
-      )
-    },
-    setFirstName (firstName) {
-      Object.defineProperty(newUser, '_firstName', {
-        writable: true
-      })
-      this._firstName = firstName
-      Object.defineProperty(newUser, '_firstName', {
-        writable: false
-      })
-    },
-    setLastName (lastName) {
-      Object.defineProperty(newUser, '_lastName', {
-        writable: true
-      })
-      this._lastName = lastName
-      Object.defineProperty(newUser, '_lastName', {
-        writable: false
-      })
-    },
-    getAge () {
-      return parseInt((new Date() - this.birthday) / 31536000000)
-    },
-    getPassword () {
-      return (
-        this._firstName[0].toUpperCase() +
-        this._lastName.toLowerCase() +
-        this.birthday.getFullYear()
-      )
+function filterBy(array, type) {
+  const newArray = [];
+  array.forEach(checkType);
+  function checkType(element) {
+    if (typeof element !== type) {
+      newArray.push(element)
     }
   }
-
-  Object.defineProperty(newUser, '_firstName', {
-    writable: false
-  })
-
-  Object.defineProperty(newUser, '_lastName', {
-    writable: false
-  })
-  console.log(newUser, newUser.getAge(), newUser.getPassword())
+  return newArray;
+  // return array.filter(checkType)
+  // always filters 0, null and other falsy values
 }
 
-createNewUser('Vasia', 'Pupkin')
+console.log(filterBy(['hello', 'world', 23, '23', null], 'string'));
+
+
+
+
+
+
+// let n;
+// function calculateFibonacci (F0, F1) {
+//   do { 
+//   n = Math.abs(prompt("Enter the number"))
+// } while (n !== null && isNaN(n));
+
+// let fibonacciNumber = [F0, F1]
+// for (let i = 2; i <= n-1; i++ ) {
+//   fibonacciNumber.push(fibonacciNumber[i-2]+fibonacciNumber[i-1])
+// }
+
+// alert(fibonacciNumber[n-1]);
+
+// }
+
+// calculateFibonacci(0,1)
+
