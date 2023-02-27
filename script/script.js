599bf5f6d84a54e33790cec599d52fe8a78f3092
Slava Tiutiunnyk <gloryon87@gmail.com>
2023-02-27 21:52:41 +0200

    JS homework 18

--- script/script.js
+++ script/script.js
@@ -1,71 +1,33 @@
 'use strict'
 
-//HW 17
-
-let className;
-let grade;
-
-const student = {
-  name: prompt("Your name"),
-  lastName: prompt("Your lastname"),
-  grades: {},
-  tabel: [],
-  getGrades() { do {
-    className = prompt("class name")
-    if (className === null) {
-      return false;
-    }
-    grade = prompt("Your grade")
-    this.grades[className] = grade
-    this.tabel.push(+grade)
-
-  } while (className !== null)
-  },
-  checkGrades(grade) {
-    return grade > 3;
-  },
-  }
-
-student.getGrades();
-
-console.log(student);
-
-if (student.tabel.every(student.checkGrades)) {
-  alert("Студент переведено на наступний курс")
-}
-
-if (student.tabel.reduce((a,b) => a + b, 0)/student.tabel.length > 7) {
-  alert("Студенту призначено стипендію")
-}
-
 // HW18
 
-// const object = {
-//   name: 'dwsdwf',
-//   eded: {
-//     number: 5,
-//     dfdef: {
-//       string: 'bla'
-//     }
-//   }
-// }
+const object = {
+  name: 'dwsdwf',
+  eded: {
+    number: 5,
+    dfdef: {
+      string: 'bla'
+    }
+  }
+}
 
-// console.log(object)
+console.log(object)
 
-// function cloneObject (object) {
-//   if (typeof object !== 'object') {
-//     return object
-//   }
-//   const newObject = Array.isArray(object) ? [] : {}
-//   for (let key in object) {
-//     newObject[key] = cloneObject(object[key])
-//   }
-//   return newObject
-// }
+function cloneObject (object) {
+  if (typeof object !== 'object') {
+    return object
+  }
+  const newObject = Array.isArray(object) ? [] : {}
+  for (let key in object) {
+    newObject[key] = cloneObject(object[key])
+  }
+  return newObject
+}
 
-// const newObject = cloneObject(object)
+const newObject = cloneObject(object)
 
-// console.log(newObject)
+console.log(newObject)
 
 // HW19
 
