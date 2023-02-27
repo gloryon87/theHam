9f3716e23da6192388feab4a42e4310cb1f73d93
Slava Tiutiunnyk <gloryon87@gmail.com>
2023-02-27 09:34:28 +0200

    JS homework 16

--- script/script.js
+++ script/script.js
@@ -1,39 +1,102 @@
 'use strict'
 
-function filterBy(array, type) {
-  const newArray = [];
-  array.forEach(checkType);
-  function checkType(element) {
-    if (typeof element !== type) {
-      newArray.push(element)
-    }
-  }
-  return newArray;
-  // return array.filter(checkType)
-  // always filters 0, null and other falsy values
+//HW 16
+
+let n;
+
+/**
+ * Calculetes n-th generalization of Fibonacci numbers
+ * @param {number} F0
+ * @param {number} F1
+ * @alerts {number}
+ */
+
+function calculateFibonacci (F0, F1) {
+  do { 
+  n = Math.abs(prompt("Enter the number"))
+} while (n !== null && isNaN(n));
+
+let fibonacciNumber = [F0, F1]
+for (let i = 2; i <= n-1; i++ ) {
+  fibonacciNumber.push(fibonacciNumber[i-2]+fibonacciNumber[i-1])
 }
 
-console.log(filterBy(['hello', 'world', 23, '23', null], 'string'));
+alert(fibonacciNumber[n-1]);
+
+}
+
+calculateFibonacci(0,1)
 
 
+// HW 17
 
+// let className;
+// let grade;
 
+// const student = {
+//   name: prompt("Your name"),
+//   lastName: prompt("Your lastname"),
+//   grades: {},
+//   tabel: [],
+//   getGrades() { do {
+//     className = prompt("class name")
+//     if (className === null) {
+//       return false;
+//     }
+//     grade = prompt("Your grade")
+//     this.grades[className] = grade
+//     this.tabel.push(+grade)
+          
+//   } while (className !== null)
+//   },
+//   checkGrades(grade) {
+//     return grade > 3;
+//   },
+//   }
 
+// student.getGrades();
 
-// let n;
-// function calculateFibonacci (F0, F1) {
-//   do { 
-//   n = Math.abs(prompt("Enter the number"))
-// } while (n !== null && isNaN(n));
+// console.log(student);
 
-// let fibonacciNumber = [F0, F1]
-// for (let i = 2; i <= n-1; i++ ) {
-//   fibonacciNumber.push(fibonacciNumber[i-2]+fibonacciNumber[i-1])
+// if (student.tabel.every(student.checkGrades)) {
+//   alert("Студент переведено на наступний курс")
 // }
 
-// alert(fibonacciNumber[n-1]);
-
+// if (student.tabel.reduce((a,b) => a + b, 0)/student.tabel.length > 7) {
+//   alert("Студенту призначено стипендію")
 // }
 
-// calculateFibonacci(0,1)
+
+
+// HW18
+
+const object = {
+    name: 'dwsdwf',
+    eded: {
+        number: 5,
+        dfdef: {
+            string: 'bla',
+        },
+    }
+}
+
+console.log(object);
+let newObject = {};
+
+function cloneObject (object) {
+    for (const key in object)
+     {
+        if (typeof object[key] !== 'object') 
+        {newObject[key] = object[key];}
+        else {
+           newObject[key] = cloneObject (object[key])
+        }       
+    }
+
+}
+
+cloneObject(object);
+console.log(newObject);
+
+
 
