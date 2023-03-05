6dab26c75d0c2a6fafd6faca654e2f31d254f379
Slava Tiutiunnyk <gloryon87@gmail.com>
2023-03-05 18:52:13 +0200

    js homework 9

--- script/script.js
+++ script/script.js
@@ -2,30 +2,46 @@
 
 // HW9
 
-const array = ["Kharkiv", "Kyiv", ["Boryspil", "Irpin"], "Odesa", "Lviv", "Dnipro"];
+const array = [
+  'Kharkiv',
+  'Kyiv',
+  ['Boryspil', 'Irpin'],
+  'Odesa',
+  'Lviv',
+  'Dnipro'
+]
 
-function insertList(array, parent = document.body) {
+/**
+ * Inserts list from array on page
+ * @param {array} array
+ * @param {Element} parent=document.body
+ * @returns {undefined}
+ */
+function insertList (array, parent = document.body) {
   const html = `<ul>${array.map(createList).join('')}</ul>`
-  parent.insertAdjacentHTML("beforeend", html);
-  function createList(elem) {
+  parent.insertAdjacentHTML('beforeend', html)
+  function createList (elem) {
     if (Array.isArray(elem)) {
-     return `<ul>${elem.map(createList).join('')}</ul>`
-    }
-    
-    else {
+      return `<ul>${elem.map(createList).join('')}</ul>`
+    } else {
       return `<li>${elem}</li>`
     }
   }
-  let timer = 3;
-  parent.insertAdjacentHTML("beforeend", `<p id="timer">${timer} seconds left</p>`);
+  let timer = 3
+  parent.insertAdjacentHTML(
+    'beforeend',
+    `<p id="timer">${timer} seconds left</p>`
+  )
   setTimeout("document.body.innerHTML = ''", 3000)
   setInterval(runTimer, 1000)
-  function runTimer() {
-    timer--;
-    document.getElementById('timer').textContent = `${timer} seconds left`}
+  function runTimer () {
+    if (timer === 1) {
+      return
+    }
+    timer--
+    document.getElementById('timer').textContent = `${timer} seconds left`
+    
+  }
 }
 
 insertList(array)
-
-
-
