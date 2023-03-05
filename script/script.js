1d0bdbd1f896cfe8977cee6e454ef8e41af49b5d
Slava Tiutiunnyk <gloryon87@gmail.com>
2023-03-05 18:37:35 +0200

    JS homework 9

--- script/script.js
+++ script/script.js
@@ -1,67 +1,31 @@
 'use strict'
 
-// HW20
+// HW9
 
-const array = [
-  { name: 'asdSad', color: 'Blue' },
-  { name: 'gghfgh', color: 'red' },
-  { name: 'hgfhfgh', color: 'white', contentType: [{ name: 'wewew', color: 'red' }] },
-  [{ name: 'wewew', color: 'bLue' }]
-]
+const array = ["Kharkiv", "Kyiv", ["Boryspil", "Irpin"], "Odesa", "Lviv", "Dnipro"];
 
-/**
- * Filters collection from array
- * @param {array} array
- * @param {string} keyWords
- * @param {boolean} all
- * @param {string} ...fields
- * @returns {array}
- */
-function filterCollection (array, keyWords, all, ...fields) {
-  function filter(element) {
-    function checkKeywords(elem) {
-      for (const field of fields) {
-        const fieldNames = field.split('.');
-        let value = element;
-    
-        for (const fieldName of fieldNames) {
-          if (value === undefined) {
-            return false;
-          }
-    
-          value = value[fieldName];
-    
-          if (Array.isArray(value)) {
-            for (const item of value) {
-              if (item.name.toLowerCase().includes(elem.toLowerCase())) {
-                return true;
-              }
-            }
-            return false;
-          } else if (typeof value === 'object' && value !== null) {
-            value = Object.values(value);
-          }
-        }
-    
-        if (value.toLowerCase().includes(elem.toLowerCase())) {
-          return true;
-        }
-      }
-    
-      return false;
+function insertList(array, parent = document.body) {
+  const html = `<ul>${array.map(createList).join('')}</ul>`
+  parent.insertAdjacentHTML("beforeend", html);
+  function createList(elem) {
+    if (Array.isArray(elem)) {
+     return `<ul>${elem.map(createList).join('')}</ul>`
     }
-    if (all === true) {
-      if (keyWords.split(' ').every(checkKeywords)) {
-        return element
-      }
-      
-    }
+    
     else {
-      if (keyWords.split(' ').some(checkKeywords)) {
-        return element
-      }
-  }}
-  return array.flat().filter((filter));
+      return `<li>${elem}</li>`
+    }
+  }
+  let timer = 3;
+  parent.insertAdjacentHTML("beforeend", `<p id="timer">${timer} seconds left</p>`);
+  setTimeout("document.body.innerHTML = ''", 3000)
+  setInterval(runTimer, 1000)
+  function runTimer() {
+    timer--;
+    document.getElementById('timer').textContent = `${timer} seconds left`}
 }
 
-console.log(filterCollection (array, 'Asdsad blue', true, 'color', 'name', 'contentType.color'));
+insertList(array)
+
+
+
