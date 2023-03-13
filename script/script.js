cdf946ff05fc951ba149c2df0b88e9111dbd6f01
Slava Tiutiunnyk <gloryon87@gmail.com>
2023-03-13 15:02:50 +0200

    js homework 12

--- script/script.js
+++ script/script.js
@@ -1,38 +1,20 @@
 'use strict'
 
-// HW11
+// HW12
 
-const inputWrappers = document.querySelectorAll('.input-wrapper')
-const form = document.querySelector('.password-form')
-const firstInput = document.getElementById('firstInput')
-const secondInput = document.getElementById('secondInput')
+const buttons = document.querySelectorAll('.btn')
+document.addEventListener('keydown', paintButton)
+const buttonsText = Array.from(buttons).map(elem =>
+  elem.innerText.toLowerCase()
+)
 
-
-inputWrappers.forEach(wrapper => {
-  wrapper.addEventListener('click', function (event) {
-    if (event.target.dataset.name === 'i') {
-      event.target.classList.toggle('fa-eye-slash')
-      for (const child of wrapper.children) {
-        if (child.type === 'password') {
-          child.type = 'text'
-        } else if (child.type === 'text') {
-          child.type = 'password'
-        }
-      }
+function paintButton (e) {
+  buttons.forEach(button => {
+    if (buttonsText.includes(e.key.toLowerCase())) {
+      button.classList.remove('btn-active')
+    }
+    if (button.innerText.toLowerCase() === e.key.toLowerCase()) {
+      button.classList.add('btn-active')
     }
   })
-})
-
-form.addEventListener('submit', function (event) {
-  event.preventDefault()
-  let passw = /^(?=.*[a-zA-Z0-9!@#$%^&*()_+])[a-zA-Z0-9!@#$%^&*()_+]{6,}$/
-  if (firstInput.value === secondInput.value && firstInput.value.match(passw)) {
-    alert('You are welcome')
-    form.reset()
-  } else {
-    alert(
-      'Потрібно ввести однакові значення, пароль має складатись щонайменше з 6 символів'
-    )
-    form.reset()
-  }
-})
+}
