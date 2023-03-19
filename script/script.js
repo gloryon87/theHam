af8905c24c3cee263e5ecd80c8f59deb7202dcff
Slava Tiutiunnyk <gloryon87@gmail.com>
2023-03-19 16:53:09 +0200

    js homework 13

--- script/script.js
+++ script/script.js
@@ -1,20 +1,109 @@
 'use strict'
 
-// HW12
+// HW13
 
-const buttons = document.querySelectorAll('.btn')
-document.addEventListener('keydown', paintButton)
-const buttonsText = Array.from(buttons).map(elem =>
-  elem.innerText.toLowerCase()
-)
+const images = document.querySelectorAll('.image-to-show')
+let imageNumber = 0
+let timerId = setInterval(showImages, 3000)
+fadeIn(images[imageNumber])
+let timeoutId = setTimeout(() => fadeOut(images[imageNumber]), 2750)
+let secondsId
+let msId
 
-function paintButton (e) {
-  buttons.forEach(button => {
-    if (buttonsText.includes(e.key.toLowerCase())) {
-      button.classList.remove('btn-active')
-    }
-    if (button.innerText.toLowerCase() === e.key.toLowerCase()) {
-      button.classList.add('btn-active')
-    }
+
+function showImages () {
+  images.forEach(image => image.classList.add('image-hidden'))
+  timeoutId = setTimeout(() => fadeOut(images[imageNumber]), 2750)
+  imageNumber += 1
+  if (imageNumber >= images.length) {
+    imageNumber = 0
+  }
+  images[imageNumber].classList.remove('image-hidden')
+  fadeIn(images[imageNumber])
+}
+
+function stopTimer() {
+  clearTimeout(timeoutId)
+  clearInterval(timerId)
+  clearInterval(secondsId)
+  clearInterval(msId)
+}
+
+/*
+виникла проблема - при виклику stopTimer в деякий момент, коли на таймері
+лишається менше секунди - спрацьовує fadeOut і зникає картинка. FadeOut
+перебиває stopTimer. Умова для fadeOut теж не спрацювала. Тож я додав функцію,
+що повертая картинці opacity 1 через 500ms (менше теж не завжди працює)
+*/
+
+function showStopedImage() {
+  setTimeout(() => {
+    images[imageNumber].style.opacity = 1
+  }, 500);
+}
+
+function addButtons () {
+  const stopShowing = document.createElement('button')
+  stopShowing.innerText = 'Припинити'
+  document.body.append(stopShowing)
+  stopShowing.className = 'button'
+  stopShowing.addEventListener('click', stopTimer)
+  stopShowing.addEventListener('click', showStopedImage)
+
+  const resumeShowing = document.createElement('button')
+  resumeShowing.innerText = 'Відновити показ'
+  document.body.append(resumeShowing)
+  resumeShowing.className = 'button'
+  resumeShowing.addEventListener('click', function () {
+    stopTimer()
+    timerId = setInterval(showImages, 3000)
+    setTimeout(() => fadeOut(images[imageNumber]), 2750)
+    seconds = 2
+    ms = 1000
+    setTimer()
   })
 }
+
+addButtons()
+
+function fadeOut (image) {
+  setTimeout(() => {
+    image.style.opacity = 0
+    image.style.transition = 'opacity 250ms'
+  }, 10)
+}
+
+function fadeIn (image) {
+  setTimeout(() => {
+    image.style.opacity = 1
+    image.style.transition = 'opacity 250ms'
+  }, 10)
+}
+
+const timer = document.createElement('div')
+let seconds = 2
+let ms = 1000
+document.body.append(timer)
+timer.innerText = `${seconds} seconds and ${ms} miliseconds left`
+
+function setTimer () {
+  secondsId = setInterval(() => {
+    if (seconds > 0) {
+      seconds -= 1
+    } else {
+      seconds = 2
+    }
+  }, 1000)
+  
+  msId = setInterval(() => {
+    if (ms > 0) {
+      ms -= 40
+    } else {
+      ms = 1000
+    }
+    timer.innerText = `${seconds} seconds and ${ms} miliseconds left`
+  }, 40)
+}
+
+setTimer()
+
